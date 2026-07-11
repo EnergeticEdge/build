5aecd863752d7e9bfc7d1aed59662aafc5ddf843
EnergeticEdge <wildwildwesty2@gmail.com>
2026-07-11 10:16:51 +0100

    Add button linking to Google

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const http = require('http');
 
 const port = process.env.PORT || 3000;
 
+const html = `<!DOCTYPE html>
+<html>
+<head><title>EE Build</title></head>
+<body>
+  <h1>EE Build is live!</h1>
+  <a href="https://www.google.com">
+    <button>Go to Google</button>
+  </a>
+</body>
+</html>`;
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('EE Build is live!');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(html);
 });
 
 server.listen(port, () => {
